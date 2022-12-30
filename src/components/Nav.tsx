a893a5f0d0762172959ca5517547342405d751eb
Ergin Deniz Kosecioglu <ergindenizkosecioglu@gmail.com>
2022-12-30 22:17:04 +0100

    Add nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+    </>
+  )
+}
+
+export default Nav
